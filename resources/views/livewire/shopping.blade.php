f945ef188fd64c5ca7f2123278a5f8d45d9d05ae
uyayash <uyayhuha@gmail.com>
2023-02-15 08:43:54 +0700

    first commit

---
+++ resources/views/livewire/shopping.blade.php
@@ -0,0 +1,34 @@
+<div>
+ <div class="container">
+   <div class="row">
+     <div class="col-3">
+        <div class="card">
+            <div class="card-header">
+                Keranjang
+            </div>
+            <div class="card-body">
+
+            </div>      
+        </div>
+        <div class="card">
+            <div class="card-header">
+                Belanjaan saya
+            </div>
+            <div class="card-body">
+
+            </div>    
+     </div>
+
+     <div class="col-9">
+ <div class="card">
+            <div class="card-header">
+                Produk
+            </div>
+            <div class="card-body">
+
+            </div>    
+     </div>
+   </div>
+ </div>
+
+</div>
