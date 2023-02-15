6869c3bea00920b907250f0eb0b9c0f505198b57
uyayash <uyayhuha@gmail.com>
2023-02-15 10:13:51 +0700

    kedua

--- resources/views/livewire/shopping.blade.php
+++ resources/views/livewire/shopping.blade.php
@@ -19,13 +19,59 @@
             </div>    
      </div>
 
-     <div class="col-9">
- <div class="card">
+    <div class="col-9">
+     <div class="card">
             <div class="card-header">
                 Produk
             </div>
             <div class="card-body">
+                <div class="row">
+                    <div class="col-3">
+                        <div class="card">
+                          <img src="{{ asset('img/hp.jpeg')}}" class="card-img-top" alt="...">
+                          <div class="card-body">
+                             <h5 class="card-title">Handphone iphone</h5>
+                             <p class="card-text">hp terbaru dari iphone</p>
+                          </div>
+                          <ul class="list-group list-group-flush">
+                             <li class="list-group-item">Rp. 15.000.000</li>
+                          </ul>
+                          <div class="card-body">
+                              <button class="btn btn-primary">BELI</button>
+                          </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="card">
+                              <img src="{{ asset('img/hp.jpeg')}}" class="card-img-top" alt="...">
+                              <div class="card-body">
+                                 <h5 class="card-title">Handphone iphone</h5>
+                                 <p class="card-text">hp terbaru dari iphone</p>
+                              </div>
+                              <ul class="list-group list-group-flush">
+                                 <li class="list-group-item">Rp. 15.000.000</li>
+                              </ul>
+                              <div class="card-body">
+                                  <button class="btn btn-primary">BELI</button>
+                              </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card">
+                                  <img src="{{ asset('img/hp.jpeg')}}" class="card-img-top" alt="...">
+                                  <div class="card-body">
+                                     <h5 class="card-title">Handphone iphone</h5>
+                                     <p class="card-text">hp terbaru dari iphone</p>
+                                  </div>
+                                  <ul class="list-group list-group-flush">
+                                     <li class="list-group-item">Rp. 15.000.000</li>
+                                  </ul>
+                                  <div class="card-body">
+                                      <button class="btn btn-primary">BELI</button>
+                                  </div>
+                                </div>
+                        
 
+                    </div>
+                </div>
             </div>    
      </div>
    </div>
